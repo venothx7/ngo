e3eb6b89bc8bba06279979c3c980b56a7ed6880a
Venoth Krishnan <venothx7@hotmail.com>
2019-06-16 23:26:25 -0400

    Add,Edit,Delete works

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,13 @@
 # urls.py
 from django.urls import path
+from .views import UserCreateView, UserUpdateView, UserDelete
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create', UserCreateView.as_view(), name='user-create'),
+    path('delete/<int:pk>', UserDelete.as_view(), name='user-delete'),
+    path('update/<int:pk>', UserUpdateView.as_view(), name='user-update'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import FormView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
-from django.http import HttpResponse
+
+from .models import User
+from .forms import UserForm
+
+
+class UserCreateView(CreateView):
+    template_name = 'user/user_create.html'
+    success_url = '/user'
+    form_class = UserForm
+
+    # this part shows the entered info in console
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
+class UserUpdateView(UpdateView):
+    template_name = 'user/user_create.html'
+    queryset = User.objects.all()
+    success_url = '/user'
+    form_class = UserForm
+
+
+class UserDelete(DeleteView):
+    model = User
+    template_name = 'user/user_delete.html'
+    success_url = ('/user')
 
 
 def index(request):
     # posts = Posts.objects.all()[:10]
     users = User.objects.all
-
     context = {
-        'users':users
+        'users': users
     }
-
     return render(request, 'user/userlist.html', context)
